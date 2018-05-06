c16bab0befed2ee4611bc48782ef6584ebc46531
AlexViy <alexviy@gmail.com>
2018-05-06 18:19:20 +0300

    added sass and made some styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Header from './Components/Header';
-import SmallRectangle from './Rectangle.png';
 import './App.css';
 
 class App extends Component {
@@ -22,7 +21,7 @@ class App extends Component {
           
             <div className="SlideText">
               <div className="IntroNav"> &#9664; &#9654; </div>
-              <h1>Boating</h1>
+              <span className="SlideTitle">Boating</span>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
               <div className="ReadMore">
                 <a href="#">Read more</a>
